7b755a14aacf07e9b4ff595a8e621943c247ac5f
Whyu9-9 <wahyuivanmahendra@gmail.com>
2023-10-23 10:25:47 +0800

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Wahyu',
+            tag: 'whyu',
+            imageUrl: '/images/img1.jpg',
+        },
+        {
+            id: 2,
+            name: 'Ivan',
+            tag: 'ivn',
+            imageUrl: '/images/img1.jpg',
+        },
+        {
+            id: 3,
+            name: 'Mahendra',
+            tag: 'mhndr',
+            imageUrl: '/images/img1.jpg',
+        },
+    ];
+}
+
+export { getData };
